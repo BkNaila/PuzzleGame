2313689bebf2e5b4a16138e9ffa6ae7f3d6ffa53
BkNaila <boukazianaila0@gmail.com>
2025-08-11 21:10:39 +0100

    Add files via upload

---
+++ GameController.java
@@ -0,0 +1,31 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.animation.TranslateTransition;
+import javafx.util.Duration;
+
+import java.io.IOException;
+
+import javafx.animation.*;
+
+public class GameController {
+
+	@FXML
+	private Button Home;
+	 @FXML
+	    private void handleHomeButton(ActionEvent event) throws IOException {
+	        Parent root = FXMLLoader.load(getClass().getResource("/application/start.fxml"));
+	        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+	        stage.setScene(new Scene(root));
+	    }
+
+}
---
+++ Main.java
@@ -0,0 +1,35 @@
+package application;
+	
+import javafx.application.Application;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.paint.Color;
+import javafx.scene.*;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.animation.*;
+
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("start.fxml"));
+			Scene scene = new Scene(root, 600, 600);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Puzzle Slinding");
+			primaryStage.setResizable(false);
+			primaryStage.centerOnScreen();
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
